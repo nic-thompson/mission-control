99e3de626ea2762ad052254c5ed31270398b7bf6
Nicolas Thompson <mail@nicolasthompson.com>
2024-03-19 16:17:05 +0000

    Adds client

---
+++ client/src/components/Footer.js
@@ -0,0 +1,18 @@
+import { Footer as ArwesFooter, Paragraph } from 'arwes';
+import Centered from './Centered';
+
+const Footer = () => {
+  return (
+    <ArwesFooter animate>
+      <Centered>
+        <Paragraph
+          style={{ fontSize: 14, margin: '10px 0', textAlign: 'center' }}
+        >
+          To infinity and beyond!
+        </Paragraph>
+      </Centered>
+    </ArwesFooter>
+  );
+};
+
+export default Footer;
