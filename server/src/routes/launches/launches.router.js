c994aff6c675af15f6cb3941cdf43867944b3f71
Nicolas Thompson <mail@nicolasthompson.com>
2024-03-20 15:59:19 +0000

    /DELETE launches

--- server/src/routes/launches/launches.router.js
+++ server/src/routes/launches/launches.router.js
@@ -3,9 +3,11 @@ const launchesRouter = express.Router();
 const {
   httpGetAllLaunches,
   httpAddNewLaunch,
+  httpAbortLaunch,
 } = require('./launches.controller');
 
 launchesRouter.get('/', httpGetAllLaunches);
 launchesRouter.post('/', httpAddNewLaunch);
+launchesRouter.delete('/:id', httpAbortLaunch);
 
 module.exports = launchesRouter;
