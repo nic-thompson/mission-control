0bbc5a6a82c52ba7df7bc4720953342308096e8c
Nicolas Thompson <mail@nicolasthompson.com>
2024-03-20 09:59:21 +0000

    Launches model

---
+++ server/src/models/launches.model.js
@@ -0,0 +1,18 @@
+const launches = new Map();
+
+const launch = {
+  flightNumber: 100,
+  mission: 'Kepler Exploration X',
+  rocket: 'Explorer IS1',
+  destination: 'Kepler-442 b',
+  launchDate: new Date('December 5, 2030'),
+  customers: ['DARPA', 'NASA', 'NOAA'],
+  upcoming: true,
+  success: true,
+};
+
+launches.set(launch.flightNumber, launch);
+
+module.exports = {
+  launches,
+};
