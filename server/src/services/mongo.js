4a4cbe86d84be542bf5ea166d5f3f0a5d945731b
Nicolas Thompson <mail@nicolasthompson.com>
2024-03-23 23:11:23 +0000

    Uses dotenv to manage secrets

--- server/src/services/mongo.js
+++ server/src/services/mongo.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
 
-const MONGO_URL =
-  process.env.MONGO_URL ||
-  'mongodb+srv://aws:yv5PDlCd4mHZ4g3x@cluster0.ozellin.mongodb.net/mission-control?retryWrites=true&w=majority&appName=Cluster0';
+require('dotenv').config();
+
+const MONGO_URL = process.env.MONGO_URL;
 
 mongoose.connection.once('connected', () => {
   console.log('Mongoose connected!');
